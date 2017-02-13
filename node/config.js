5eb0021c0d2125b13b2e1393780bf7366431be77
mbrav <mbrav@protonmail.com>
2017-02-12 19:10:52 -0500

    ****

--- node/config.js
+++ node/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	consumer_key:			"YOUR API CODE GOES HERE",
-	consumer_secret:		"YOUR API CODE GOES HERE",
-	access_token:			"YOUR API CODE GOES HERE",
-	access_token_secret:	"YOUR API CODE GOES HERE",
+	consumer_key:			"HqGZ9oSAePyvjNdzpN90F9cpI",
+	consumer_secret:		"YMJzLsueBnBgbsqdvKO3H7HQEm4tJPQbyNUyQ3BKleiVmCjR5S",
+	access_token:			"896480534-zxP3zjChBvs7hyxgfWrSa4U6iw0m4GiFfweIyC0K",
+	access_token_secret:	"YPFpQWdHbZqIApkqQ06Nm9bkTBBymoqXY730sK84SuRKQ",
 };
