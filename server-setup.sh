4b66a8cbd1e571ab0abb93e69f2dc3a8ed21c9be
mbrav <mbrav@protonmail.com>
2021-12-17 14:35:28 +0300

    improved server setup script

--- server-setup.sh
+++ server-setup.sh
@@ -3,31 +3,37 @@
 #Configures Procesing to run in "headless" mode
 
 echo "Configuring OssacipBot server..."
+url="https://github.com/processing/processing/releases/download/processing-0270-3.5.4/processing-3.5.4-linux64.tgz"
+tar_file="processing.tgz"
+folder="./processing"
+processing="${folder}/processing-java"
+lib_path="${folder}/sketchbook/libraries/"
 
-#Download Processing
+app_export_path="bot/app"
+
 echo "Downloading Processing 3 into home directory..."
-wget https://github.com/processing/processing/releases/download/processing-0270-3.5.4/processing-3.5.4-linux64.tgz
+wget $url -O $tar_file
 
 echo "Unpacking Processing..."
-tar -xvzf processing-3.5.4-linux64.tgz
-rm processing-3.5.4-linux64.tgz
+mkdir $folder
+tar -xvzf $tar_file -C $folder --strip-components=1
+rm $tar_file 
 
-#Install Processing dependencies
 echo "Installing Processing 3 libraries..."
-mkdir -p ~/sketchbook/libraries/
+mkdir -p $lib_path
 wget https://github.com/hamoid/video_export_processing/releases/download/v23/VideoExport-23.zip
-unzip VideoExport-23.zip -x "**/examples/*" -d ~/sketchbook/libraries/
+unzip VideoExport-23.zip -x "**/examples/*" -d $lib_path
 rm VideoExport-23.zip
 
-#Create a complied executable Processing Applet
+
 echo "Creating an executable Processing applet."
-./processing-3.5.4/processing-java --sketch=Ossacip --output=bot/app --force --export
+$processing --sketch=Ossacip --output=$app_export_path --force --export
 
-#Install Xvfb a screen emulater and dependencies
-#Needed for displaying Processing apps on computers without screens
-sudo apt-get install xvfb libxrender1 libxtst6 libxi6 default-jre
+# #Install Xvfb a screen emulater and dependencies
+# #Needed for displaying Processing apps on computers without screens
+# sudo apt-get install xvfb libxrender1 libxtst6 libxi6 default-jre
 
-#Make the generator script executable
-sudo chmod +x bot/generate.sh
+# #Make the generator script executable
+# sudo chmod +x bot/generate.sh
 
 echo "OssacipBot is set up!"
